aaa0efb6e767865e9accb9fb38037653ea6fa82d
NFKRZZ <43969824+NFKRZZ@users.noreply.github.com>
2019-03-05 12:27:23 -0800

    final

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -98,5 +98,17 @@ public class IntArrayWorker
       }
     }
   }
+  public int getCount(int n)
+  {
+    for(int i =1; i<matrix.length;i++)
+    {
+        for(int j = 0; j<matrix[0].length; j++)
+        {
+        
+        }
+    }
+    return 1;  
+    
+  }
  
 }
